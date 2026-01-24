aa47dd759479c94c13653ee35d3dab6539d4a18e
Kezia Avila Moreira Messias <kezia.messias1990@gmail.com>
2026-01-24 00:53:49 -0300

    subindo atualizações na nova branch

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('Webapp deve estar online', async ({ page }) => {
+test('Webapp deve estar online, criando uma nova branch no github e fazendo um pull request' , async ({ page }) => {
+  const gitHub = new GitHub(page);
   await page.goto('http://localhost:5173');
 
   await expect(page).toHaveURL('http://localhost:5173');
+  await gitHub.createNewGitHubBranch('feature/online');
+  await gitHub.pushToGitHub('feature/online');
+  await gitHub.createPullRequest('feature/online');
 });
-
-
